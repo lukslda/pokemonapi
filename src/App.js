cc01162c7b57f19693459593d6491d2d4b650f72
lukslda <lukslda@gmail.com>
2020-06-25 22:48:55 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useQuery } from '@apollo/react-hooks';
+import gql from "graphql-tag";
+
+const EXAMPLE_QUERY = gql`
+{
+  pokemons(first: 10) {
+    id
+    number
+    name,
+    image
+  }
+}`;
 
 function App() {
+  const { data, loading, error } = useQuery(EXAMPLE_QUERY);
+
+  if (loading) return <p>Still loading..</p>;
+  if (error) return <p>There is an error!</p>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="container">
+        <h1>Pokemon API</h1>
+        {data && data.pokemons &&
+          data.pokemons.map((pokemon, index) => (
+
+            <div key={index}>
+
+              <img src={pokemon.image} />
+              <div>
+                <h3>{pokemon.name}</h3>
+
+              </div>
+            </div>
+
+          ))}
+      </div>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'https://graphql-pokemon.now.sh/'
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
